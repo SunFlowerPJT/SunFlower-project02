06f05a0cb024d2d328ca095717da87330aab080c
jeongyun11 <uwn1004@gmail.com>
2023-04-11 18:40:55 +0900

    startproject_structure

---
+++ SunFlower_site/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SunFlower_site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SunFlower_site.settings')
+
+application = get_asgi_application()
