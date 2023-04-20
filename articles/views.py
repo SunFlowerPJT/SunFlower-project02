1da171ff1c3376b8bc13b38119da7b76c551d72f
jeongyun11 <uwn1004@gmail.com>
2023-04-20 19:03:06 +0900

    add articles/comments

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from .forms import ArticleForm
+from .models import Comment
+from .forms import ArticleForm, CommentForm
 
 # Create your views here.
 def index(request):
@@ -13,8 +14,12 @@ def index(request):
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm()
+    comments =  article.comment_set.all() # Comment.objects.filter(article = article.pk)
     context = {
         'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
     }
     return render(request, 'articles/detail.html', context)
 
@@ -53,3 +58,26 @@ def update(request, article_pk):
         'article': article,
     }
     return render(request, 'articles/update.html', context)
+
+def comments_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm(request.POST)
+
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment_form.save()
+        return redirect('articles:detail', article.pk)
+    
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+    }
+
+    return render(request, 'articles/detail.html', context)
+
+
+def comments_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk = comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
