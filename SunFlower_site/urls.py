f656f66204f52ad461f7259f75616935a3cd227b
jeongyun11 <uwn1004@gmail.com>
2023-04-21 19:05:54 +0900

    test main

--- SunFlower_site/urls.py
+++ SunFlower_site/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
