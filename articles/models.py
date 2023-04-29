f830ee763f1d1740e81c30ce6b1e21972c8392c8
zzgh06 <zzgh06@naver.com>
2023-04-30 01:40:36 +0900

    Add like

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Article(models.Model):
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
     title = models.CharField(max_length=20)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
--- articles/views.py
+++ articles/views.py
@@ -81,3 +81,22 @@ def comments_delete(request, article_pk, comment_pk):
     comment = Comment.objects.get(pk = comment_pk)
     comment.delete()
     return redirect('articles:detail', article_pk)
+
+
+def comments_likes(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if comment.like_users.filter(pk=request.user.pk).exists():
+        comment.like_users.remove(request.user)
+    else:
+        comment.like_users.add(request.user)
+    return redirect('articles:detail', article_pk)
+
+
+def likes(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if article.like_users.filter(pk=request.user.pk).exists():
+        article.like_users.remove(request.user)
+    else:
+        article.like_users.add(request.user)
+    return redirect('articles:index', article_pk)
+
